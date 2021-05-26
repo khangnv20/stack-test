f0cdd84216617fde8d72964bd171eb7c5675a3ea
Khang <khangnvgcc19118@fpt.edu.vn>
2021-05-26 15:24:10 +0700

    Add project files.

---
+++ Exercise1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Numerics;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //console.writeline("enter date: ");
+            //string date1 = console.readline();
+            //datetime datetime1;
+            //try
+            //{
+            //    datetime1 =  datetime.parseexact(date1, "d-m-yyyy", cultureinfo.invariantculture);
+            //    console.writeline(datetime1.dayofweek);
+            //}
+            //catch (exception e)
+            //{
+            //    console.writeline(e.message);
+            //}
+            cd:Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Gray;
+            
+            Console.WriteLine("Insert your number");
+            BigInteger f = int.Parse(Console.ReadLine());
+            BigInteger fac = 1;
+            for (int i = 1; i <= f; i++)
+            {
+                fac*= i;
+            }
+            Console.WriteLine(fac);
+            Console.ResetColor();
+            goto cd;
+        }
+        
+    }
+}
