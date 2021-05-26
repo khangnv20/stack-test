3bcee1d14f6504e202b85080b4ef0f3a95c5dbb8
Khang <khangnvgcc19118@fpt.edu.vn>
2021-05-26 15:24:10 +0700

    Add project files.

---
+++ Exercise1/Dice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise1
+{
+    internal class Dice
+    {
+        public int Sides { get; set; }
+        public Dice()
+        {
+            this.Sides  =  12;
+        }
+
+        public int Roll()
+        {
+            Random rnd = new Random();
+            return rnd.Next(1, Sides+1);
+        }
+    }
+}
